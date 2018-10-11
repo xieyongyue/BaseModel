9a9f46870c2b9ed363367e140157b677e81bbae2
xieyongyue <xieyongyueios@163.com>
2018-10-11 19:11:36 +0800

    第一次提交

---
+++ BaseDemo/BaseDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseDemo
+//
+//  Created by xyy on 2018/10/11.
+//  Copyright © 2018年 谢勇岳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BaseDemo/BaseDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BaseDemo
+//
+//  Created by xyy on 2018/10/11.
+//  Copyright © 2018年 谢勇岳. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
